9532dafb88b0b7ecd14c0230d7409728bdeedb7e
Sarah O'Dell <sarah.mo26@gmail.com>
2023-09-04 19:21:20 -0600

    find info for an account number

--- from_csv.py
+++ from_csv.py
@@ -1,6 +1,7 @@
 import csv
 import json
-# pull in the csv
+
+# pull in the csv - for now, just using test file in project, will need to implement importing one
 imported_csv = '/Users/sarah/Desktop/Development/from-csv-python/stockCsv.csv'
 def convert_csv(file):
     csv_file = open(file, 'r', encoding='utf-8-sig')
@@ -8,14 +9,16 @@ def convert_csv(file):
     data = [row for row in json_of_csv]
     csv_file.close()
 
-    return data[0]
+    stocks_for_account = find_stocks_for_account(data, '1')
+    return stocks_for_account
     
-
-# print(convert_csv(imported_csv))
 # parse out the csv
-
-#handle if an account doesn't have 10 stocks
-
-#print out results
-
-#ui
+# find all accounts related to the account number
+def find_stocks_for_account(data,accountNum):
+    for account in data:
+        for key, value in account.items():
+            if value == accountNum:
+                return account
+            else:
+                return 'No account found'
+print(convert_csv(imported_csv))
