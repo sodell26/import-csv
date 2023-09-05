193a53319ce3cf4cfa4914fc1806a9635be7c631
Sarah O'Dell <sarah.mo26@gmail.com>
2023-09-04 19:28:11 -0600

    added search for accounts with stock names

--- from_csv.py
+++ from_csv.py
@@ -9,8 +9,9 @@ def convert_csv(file):
     data = [row for row in json_of_csv]
     csv_file.close()
 
-    stocks_for_account = find_stocks_for_account(data, '1')
-    return stocks_for_account
+    # stocks_for_account = find_stocks_for_account(data, '1')
+    accounts_with_stock = find_accounts_with_stocks(data, 'GOOG')
+    return accounts_with_stock
     
 # parse out the csv
 # find all accounts related to the account number
@@ -21,4 +22,14 @@ def find_stocks_for_account(data,accountNum):
                 return account
             else:
                 return 'No account found'
+
+def find_accounts_with_stocks(data, stockName):
+    accounts = []
+    for stock in data:
+        for key, value in stock.items():
+            if value == stockName:
+                accounts.append(stock['account_number'])
+            
+    return accounts
+
 print(convert_csv(imported_csv))
